65783e083ea1be60267f2c722217ff9b32bcb9b2
baishen1 <3457709014@qq.com>
2019-10-15 13:05:33 +0800

    work4

--- work5/work5.js
+++ work5/work5.js
@@ -1,5 +1,14 @@
 //9x9的空表，情自行编写这段代码
-
+str = '<table border="1">'
+for(let i=1;i<10;++i){
+    str += '<tr>'
+    for(var j=1;j<10;++j){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table1').innerHTML = str
 
 //倒九九乘法空表
 str = '<table border="1">'
@@ -14,3 +23,13 @@ str += '</table>'
 document.getElementById('table2').innerHTML = str
 
 //倒九九乘法表 ，情自行编写这段代码
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str +='<td>' + j + '*' + i + '=' + ( j * i ) + '</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str
