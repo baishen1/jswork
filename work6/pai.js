ea72065d707e9556394c84892fda7ab3e838d765
baishen1 <3457709014@qq.com>
2019-10-17 09:56:37 +0800

    pai.js

---
+++ work6/pai.js
@@ -0,0 +1,34 @@
+//"计算"按钮点击处理函数
+function calculate(){
+    //读取阶数输入框的数值
+    let num = document.getElementById('').value
+    num = parseInt (num)&&Number(num)
+    if(!isNaN(num)){
+        //设置圆周率输入框的值
+        document.getElementById('').value = pi()
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let   ){
+
+    }
+    return sum
+}
+function oddFactorial(n){
+    let sum = 1
+    for(let i=1  ){
+
+    }
+    return sum
+}
+function pi(num){
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let dividend = factorial()
+        let divisor = oddFactorial()
+
+    }
+    return 
+}
+calculate
