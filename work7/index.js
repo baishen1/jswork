ef9d1a338fab1e511fbc9b3dabc07d1c35260928
baishen1 <3457709014@qq.com>
2019-10-21 11:12:05 +0800

    baishen1

--- work7/index.js
+++ work7/index.js
@@ -2,7 +2,7 @@ function sort(method){
     //读取待排序数据
     let origin = document.getElementById('origin').value
     let logs
-    console.log
+    console.log(method)
     //根据传入排序的方法调用不同的排序函数
     if(method=='bubble'){
         //条用bubbleSort(）冒泡排序函数获取排序结果，
